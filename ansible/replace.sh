ce2fdc173e40dfbf65c9d079f651cb43f23400c3
emna-dammak <emna.dammak@insat.ucar.tn>
2025-04-26 13:12:35 +0100

    fix: removed -state option

--- ansible/replace.sh
+++ ansible/replace.sh
@@ -2,12 +2,12 @@
 # Script pour mettre à jour l'inventaire Ansible avec l'IP et les identifiants de la VM Azure
 
 # Récupérer l'IP publique et le nom d'utilisateur depuis les sorties Terraform
-IP_ADDRESS=$(terraform output -raw public_ip_address -state=../terraform/terraform.tfstate)
-USERNAME=$(terraform output -raw vm_username  -state=../terraform/terraform.tfstate)
-PASSWORD=$(terraform output -raw vm_password  -state=../terraform/terraform.tfstate)
+IP_ADDRESS=$(terraform output -raw public_ip_address )
+USERNAME=$(terraform output -raw vm_username )
+PASSWORD=$(terraform output -raw vm_password)
 
 # Créer le fichier d'inventaire
-cat > inventory.ini << EOF
+cat > ansible/inventory.ini << EOF
 [web]
 azure-vm ansible_host=$IP_ADDRESS ansible_user=$USERNAME ansible_password=$PASSWORD ansible_connection=ssh ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 EOF
